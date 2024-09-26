e23c1b070f2c3114911d84f6c25ad0a8da4a434d
Ahmed Samir <ahmed123.as27@gmail.com>
2024-09-26 20:16:04 +0400

    edited for fall semester

--- main.py
+++ main.py
@@ -4,12 +4,32 @@ import openpyxl
 import os
 from colorist import red, effect_underline, Color, effect_bold
 
-# dictionary where the attendance files at
-directory = "D:\\astro\\Astro\\uni tutoring\\M110\\attendance"
+def filter_file(filename):
+    #filters file names
+    if not filename.startswith("~") and filename.endswith("xlsx"):
+        return True
+    return False
 
-# getting week number to record attendance for the corresponding week
-target_week = int(input("Enter week number: "))
+current_season = "fall"
+current_semester = "2024-2025"
 
+while True:
+    # which module 269 or 110
+    target_module = int(input("Enter Module number: "))
+    modules = [269, 110]
+    if target_module not in modules:
+        print(f"Please choose appropriate module between the specified choices: {modules}")
+        continue
+    # getting week number to record attendance for the corresponding week
+    target_week = int(input("Enter week number: "))
+    week_range = range(1, 15)
+    if target_week not in week_range:
+        print(f"Please choose appropriate range between the specified choice: {week_range}")
+        continue
+    # dictionary where the attendance files at
+    directory = f"D:\\astro\\Astro\\uni tutoring\\M{target_module}\\attendance\\{current_semester}\\{current_season}"
+    print(f"Fetch from directory: {directory}")
+    break
 # using this list for displaying purposes once the students is recorded as attended
 # it is also used to write the data to a temp file in case of corruption
 # it stores a tuple(studentId, sectionNumber, StudentName)
@@ -23,84 +43,87 @@ while True:
         break
 
     # using this counter to indicate that the student id wasn't found in
-    # 4 excell files if this counter value is 4
+    # the excell files if this counter value is 2 (failure_value) which is the number of excel files we are searching in
+    failure_value = 2
     notFoundCounter = 0
 
+    attendance_files = list(filter(filter_file, os.listdir(directory)))
     # looping through files within the directory and opening only xlsx files
-    for filename in os.listdir(directory):
-        if filename.endswith("xlsx"):
-            # getting different information about the file first
-            # all the section except section 62 has different format
-            # so there are different values for section 62 specifically
-            isSection62 = '62' in filename
-            id_row = 10 if isSection62 else 6
-            id_column = 3
-            current_week = target_week - 1 if isSection62 else target_week
-            week_column = id_column + 3 + current_week if isSection62 else id_column + 3 + current_week
-            name_column = id_column + 1
-            section_number = filename[10:13]
+    for filename in attendance_files:
+        # getting different information about the file first
+        # has an extra column so I am checking before finding cells and committing changes
+        is269 = 269 == target_module
+        # id starts within the 6th row
+        id_row =  6
+        # id found in column 2 in all files
+        id_column = 2
+        # 269 files has an extra column
+        week_column = 5 + target_week if is269 else 4 + target_week
 
-            # opening the file and activating the current sheet
-            try:
-                wb = openpyxl.load_workbook(os.path.join(directory, filename))
-                sheet = wb.active
-            except PermissionError:
-                red("Error while Opening files: Please make sure attendance files are closed while taking the "
-                    "attendance")
-                quit(-1)
+        name_column = id_column + 1
+        section_number = filename[10:12]
+
+        # opening the file and activating the current sheet
+        try:
+            wb = openpyxl.load_workbook(os.path.join(directory, filename))
+            sheet = wb.active
+        except PermissionError:
+            red("Error while Opening files: Please make sure attendance files are closed while taking the "
+                "attendance")
+            quit(-1)
 
 
-            # this indicator used to exit the loops once student's id is found and marked attended
-            # it is useful to immediately save the changes once the id found to avoid redundancy
-            changeMade = False
+        # this indicator used to exit the loops once student's id is found and marked attended
+        # it is useful to immediately save the changes once the id found to avoid redundancy
+        changeMade = False
 
-            # looping through each record within the table
-            # starting from the rows and columns where student ids are found
-            # again to avoid redundancy
-            for record in sheet.iter_rows(min_row=id_row, min_col=id_column, max_col=id_column):
+        # looping through each record within the table
+        # starting from the rows and columns where student ids are found
+        # again to avoid redundancy
+        for record in sheet.iter_rows(min_row=id_row, min_col=id_column, max_col=id_column):
 
-                # looping through each cell
-                # and comparing its value with the target student's id
-                for cell in record:
-                    # once found we get the current row and column to move from there
-                    # to get more information like the name
-                    if cell.value == target_id:
-                        row = cell.row
-                        column = cell.column
+            # looping through each cell
+            # and comparing its value with the target student's id
+            for cell in record:
+                # once found we get the current row and column to move from there
+                # to get more information like the name
+                if cell.value == target_id:
+                    row = cell.row
+                    column = cell.column
 
-                        # marking the student as attended for the specific week
-                        sheet.cell(row=row, column=week_column, value="p")
-                        # getting students name
-                        name = sheet.cell(row, name_column).value
-                        # displaying to console that the operation was successful with bold text
-                        effect_bold(
-                            f"Name: {name}\nSection: {section_number}\nhas been successfully marked as attended for "
-                            f"week {target_week}\n\n")
-                        # here we set the changeMade to true, so we can immediately stop looping
-                        # and ask for the next student's id
-                        changeMade = True
-                        # adding the record to list for the reasons set earlier
-                        recorded_students.append((target_id, section_number, name))
-                        break
-                if changeMade:
+                    # marking the student as attended for the specific week
+                    sheet.cell(row=row, column=week_column, value="p")
+                    # getting students name
+                    name = sheet.cell(row, name_column).value
+                    # displaying to console that the operation was successful with bold text
+                    effect_bold(
+                        f"Name: {name}\nSection: {section_number}\nhas been successfully marked as present for "
+                        f"week {target_week}\n\n")
+                    # here we set the changeMade to true, so we can immediately stop looping
+                    # and ask for the next student's id
+                    changeMade = True
+                    # adding the record to list for the reasons set earlier
+                    recorded_students.append((target_id, section_number, name))
                     break
             if changeMade:
-                # saving the file and resetting the counter, so it doesn't reach 4
-                # indicating that student's id was found and changes has been made
-                try:
-                    wb.save(os.path.join(directory, filename))
-                    notFoundCounter = 0
-                except PermissionError:
-                    red(f"Error while Saving files: {name} ({target_id}) was not recorded as an attendee, please make sure attendance files are closed while taking the attendance")
                 break
-            else:
-                # if the id is not found within a file then it will be incremented
-                # until it reaches 4 indicating that the id is not within any file
-                # then we counter
-                notFoundCounter += 1
+        if changeMade:
+            # saving the file and resetting the counter, so it doesn't reach 4
+            # indicating that student's id was found and changes has been made
+            try:
+                wb.save(os.path.join(directory, filename))
+                notFoundCounter = 0
+            except PermissionError:
+                red(f"Error while Saving files: {name} ({target_id}) was not recorded as an attendee, please make sure attendance files are closed while taking the attendance")
+            break
+        else:
+            # if the id is not found within a file then it will be incremented
+            # until it reaches 4 indicating that the id is not within any file
+            # then we counter
+            notFoundCounter += 1
 
         # if id is not found within the 4 files
-        if notFoundCounter == 4:
+        if notFoundCounter == failure_value:
             red(f"Student with id {target_id} is not found")
             notFoundCounter = 0
 
@@ -127,7 +150,7 @@ def writeRecordToFile():
     # construct the file name without invalid characters
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    file_name = f"Week_{target_week}_{timestamp}.txt"
+    file_name = f"Week{target_week}_M{target_module}_{timestamp}.txt"
 
     with open(os.path.join(path, file_name), 'w') as f:
         for index in range(len(recorded_students)):
